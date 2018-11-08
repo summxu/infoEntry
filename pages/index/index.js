e71b80ba1bbb5d7938eaa07f61534d4ee1cea78d
Chenxu <chenxu4012@foxmial.com>
2018-11-08 18:14:50 +0800

    接口对接

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,43 +2,38 @@
 const app = getApp()
 Page({
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    infoList: [
-      { idcard: '370111223344556677', name: '黎明' },
-      { idcard: '370111223344556677', name: '黎明' },
-      { idcard: '370111223344556677', name: '黎明' },
-      {idcard:'370111223344556677',name:'黎明'}
-    ]
+    infoList: []
+  },
+  onReady: function() {
+
   },
   onLoad: function(options) {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res)
+        wx.request({
+          url: app.globalData.url + 'jscode/' + res.code,
+          success: (res) => {
+            app.globalData.openid = res.data.openid
+            wx.request({
+              url: app.globalData.url + 'list/',
+              method: 'POST',
+              data: {
+                opid: app.globalData.openid
+              },
+              success: (res) => {
+                this.setData({
+                  infoList:res.data
+                })
+                // console.log(this.data.infoList)
+              }
+            })
+          }
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
+
   },
   getUserInfo: function(e) {
     console.log(e)
